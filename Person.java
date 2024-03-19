91922619d204f00e44031a627b104c84cfbb6200
kongkange <kongkange@naver.com>
2024-03-19 10:16:35 +0900

    set,get 추가[검사자]

--- Person.java
+++ Person.java
@@ -1,6 +1,23 @@
 class Person{
 	String name;
 	int age;
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public void setAge(int age){
+		this.age = age;
+	}
+
+	public int getAge(){
+		return age;
+	}
+
 	public void sayHello(){
 		System.out.println("Hello, "+name);
 	}
--- Rect.java
+++ Rect.java
@@ -1,6 +1,23 @@
 class Rect{
 	int width;
-	int height
+	int height;
+
+	public void setWidth(int width){
+		this.width = width;
+	}
+
+	public int getWidth(){
+		return width;
+	}	
+
+	public void setHeight(int height){
+		this.height = height;
+	}
+
+	public int getHeight(){
+		return height;
+	}
+
 	pulbic int getArea(){
 		return width * height;
 	}
